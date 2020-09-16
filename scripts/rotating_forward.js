c0692a189a89b791459848b106db1dfd44460007
ideck802 <ideck802@gmail.com>
2020-09-15 19:10:13 -0500

    theme fixing, project header, and added about button

--- scripts/rotating_forward.js
+++ scripts/rotating_forward.js
@@ -15,7 +15,7 @@ rotators.forEach(rotator => {
   observer.observe(rotator);
 });
 
-const rotators2 = document.querySelectorAll('.rotating_title');
+const rotators2 = document.querySelectorAll('.rotating_title2');
 
 var observer2 = new IntersectionObserver(function(entries) {
   entries.forEach(entry => {
@@ -25,8 +25,24 @@ var observer2 = new IntersectionObserver(function(entries) {
       entry.target.classList.remove('rotate-forward_title');
     }
   });
-}, { threshold: [0.8], rootMargin: ['-80px 1px 1px 1px'] });
+}, { threshold: [1] });
 
 rotators2.forEach(rotator => {
   observer2.observe(rotator);
+});
+
+const rotators3 = document.querySelectorAll('.rotating_title');
+
+var observer3 = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting === true) {
+      entry.target.classList.add('rotate-forward_title');
+    } else {
+      entry.target.classList.remove('rotate-forward_title');
+    }
+  });
+}, { threshold: [0.8], rootMargin: ['-80px 1px 1px 1px'] });
+
+rotators3.forEach(rotator => {
+  observer3.observe(rotator);
 });
